b1d18d3acc62ebb35529673fa5718c9727f567d8
Mustafa Mansour <mustafa.k.mansour@gmail.com>
2019-10-28 21:21:46 +1100

    Section1 completed

--- src/index.js
+++ src/index.js
@@ -1,12 +1,24 @@
+/*
+  import: ES2015 Module
+  require: CommonJS Module
+*/
+
 import React from 'react';
 import ReactDOM from 'react-dom';
-import './index.css';
-import App from './App';
-import * as serviceWorker from './serviceWorker';
 
-ReactDOM.render(<App />, document.getElementById('root'));
 
-// If you want your app to work offline and load faster, you can change
-// unregister() to register() below. Note this comes with some pitfalls.
-// Learn more about service workers: https://bit.ly/CRA-PWA
-serviceWorker.unregister();
+/* 
+  React components are Functions or CLasses to:
+    * produce content
+    * handle user feedback/interaction
+*/
+
+const App = () => {
+  // function component
+  return <div>Hello World!</div>;
+}
+
+ReactDOM.render(
+  <App />,
+  document.querySelector('#root')
+);
