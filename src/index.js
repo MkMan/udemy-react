851454ca1b80b8c916a6dd7de05a4b4d4429df97
Mustafa Mansour <mustafa.k.mansour@gmail.com>
2019-10-30 20:39:18 +1100

    Section 5 done

--- src/index.js
+++ src/index.js
@@ -1,26 +1,51 @@
-/*
-  import: ES2015 Module
-  require: CommonJS Module
-*/
-
 import React from 'react';
 import ReactDOM from 'react-dom';
 
-
-/* 
-  React components are Functions or CLasses to:
-    * produce content
-    * handle user feedback/interaction
-*/
-
-const App = () => {
-  /* 
-    JSX (the content of the render method) gets compiled to JS call to
-    React.createElement 
+class App extends React.Component {
+  /*
+    React.Component contains other methods that can be used in sub classes
   */
 
-  // function component
-  return <div>Hello World!</div>;
+  constructor(props) {
+    super(props); // should always do this
+
+    /* 
+      one way to init state is to use the constructor 
+    */
+    this.state = {
+      lat: null,
+      errorMsg: ''
+    };
+
+    window.navigator.geolocation.getCurrentPosition(
+      (position) => {
+        /*
+          setState is inherited from React.Component
+          MUST be called to update state. Should NEVER update state directly
+          !! Updating state updates the properties specified and does not replace the whole state !!
+        */
+        this.setState({ lat: position.coords.latitude });
+      },
+      (error) => {
+        this.setState({ errorMsg: error.message })
+      }
+    );
+  }
+
+  /*
+    must have this method
+    it gets called frequently so should be as light as possible
+  */
+  render() {
+    // conditional rendering
+    if (this.state.errorMsg && !this.state.lat) {
+      return <div>Error: {this.state.errorMsg}</div>;
+    } else if (!this.state.errorMsg && this.state.lat) {
+      return <div>Latitude: {this.state.lat}</div>;
+    } else {
+      return <div>Loading</div>;
+    }
+  }
 }
 
 ReactDOM.render(
