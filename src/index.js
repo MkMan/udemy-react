d5bf48e3f9626e16c951a176db0b3ff8504dca24
Mustafa Mansour <mustafa.k.mansour@gmail.com>
2019-10-31 22:51:34 +1100

    Section 7 done

--- src/index.js
+++ src/index.js
@@ -1,27 +1,6 @@
-/*
-  import: ES2015 Module
-  require: CommonJS Module
-*/
-
 import React from 'react';
 import ReactDOM from 'react-dom';
-
-
-/* 
-  React components are Functions or CLasses to:
-    * produce content
-    * handle user feedback/interaction
-*/
-
-const App = () => {
-  /* 
-    JSX (the content of the render method) gets compiled to JS call to
-    React.createElement 
-  */
-
-  // function component
-  return <div>Hello World!</div>;
-}
+import App from './components/App';
 
 ReactDOM.render(
   <App />,
